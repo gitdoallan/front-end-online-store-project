6f834958e48e412080ee7164b380a382a1513c0b
gitdoallan <mailtoallan@icloud.com>
2022-03-11 19:38:35 -0300

    loucuras no requisito 10

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -6,8 +6,12 @@ export default class ShoppingCart extends React.Component {
     super();
     this.state = {
       products: [],
+      productQty: [],
     };
     this.initialState = this.initialState.bind(this);
+    this.increaseQty = this.increaseQty.bind(this);
+    this.decreaseQty = this.decreaseQty.bind(this);
+    this.removeItem = this.removeItem.bind(this);
   }
 
   componentDidMount() {
@@ -18,13 +22,38 @@ export default class ShoppingCart extends React.Component {
     const { addedToCart } = this.props;
     addedToCart.forEach(async (id) => {
       const result = await api.getProductDetails(id);
-      this.setState((previousState) => ({ products: [...previousState.products, result] }));
+      this.setState((previousState) => (
+        {
+
+          products: [...previousState.products, result],
+          productQty: [...previousState.productQty, { [result.id]: 1, id: result.id }],
+        }));
     });
   }
 
+  increaseQty(id) {
+    const { productQty } = this.state;
+    const findProduct = productQty.find((element) => element.id === id);
+    const index = productQty.indexOf(findProduct);
+    console.log(findProduct);
+    console.log(index);
+    this.setState((prevState) => [ {
+        productQty: [...prevState.productQty,
+          { [findProduct.id] : prevState.productQty[index][id] + 1 } ]
+        }])
+  }
+
+  decreaseQty(id) {
+    console.log('menos um');
+  }
+
+  removeItem(id) {
+    console.log('item removido');
+  }
+
   render() {
-    const { products } = this.state;
-    console.log(products);
+    const { products, productQty } = this.state;
+    console.log(this.state);
     return (
       <div className="cart-products">
         {products.length === 0
@@ -37,6 +66,12 @@ export default class ShoppingCart extends React.Component {
                 Preço:
                 {element.price}
               </p>
+              <p>
+                Quantidade: { productQty[element.id] }
+              </p>
+              <button type="button" onClick={ () => this.increaseQty(element.id) }>Aumentar 1</button>
+              <button type="button" onClick={ () => this.decreaseQty(element.id) }>Diminuir 1</button>
+              <button type="button" onClick={ () => this.removeItem(element.id) }>Remover do carrinho</button>
               <p data-testid="shopping-cart-product-quantity">1</p>
             </div>
           ))}
